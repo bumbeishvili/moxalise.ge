9ed27057c0a737cd6da51e406fcc1ea89cabacec
bumbeishvili <me@davidb.dev>
2025-03-01 02:42:29 +0400

    fix renew button

--- assets/js/app.js
+++ assets/js/app.js
@@ -495,13 +495,11 @@ function createSidebarCards() {
           return `<p><strong>${key}:</strong> ${formatValue(value)}</p>`;
         })
         .join('')}
-                ${item.lat && item.lon
-        ? `
+                ${`
                 <div class="card-actions">
                     ${item.id ? `<button id="card-notification-btn-${index}" onclick="event.stopPropagation(); sendNotification('card-notification-btn-${index}')" class="card-notification-btn" style="position: relative; z-index: 3500;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right: 5px;"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>განაახლე ინფორმაცია!</button>` : ''}
                 </div>
                 `
-        : ''
       }
             </div>`;
 
